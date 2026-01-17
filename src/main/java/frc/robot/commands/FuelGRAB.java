b5931000d7dfb1582d7a70e5d0dc424a3cf48b8b
Your Name <your.email@example.com>
2026-01-17 15:39:13 -0600

    changed names for hopper

--- src/main/java/frc/robot/commands/FuelGRAB.java
+++ src/main/java/frc/robot/commands/FuelGRAB.java
@@ -61,7 +61,7 @@ public class FuelGRAB extends Command {
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
-        return false;
+        return true;
     }
 
     @Override
