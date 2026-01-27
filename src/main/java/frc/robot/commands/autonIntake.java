884c8d816dcb66b0eae939025a0cca6747ef653b
Paxton Reeve <paxtonreeve@gmail.com>
2026-01-26 19:19:00 -0600

    added auton commands

---
+++ src/main/java/frc/robot/commands/autonIntake.java
@@ -0,0 +1,41 @@
+package frc.robot.commands;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Hopper;
+
+public class autonIntake extends Command {
+    private final Hopper m_hopper;
+    private final Timer m_timer = new Timer();
+    private final double m_duration;
+
+    public autonIntake(Hopper hopper, double duration) {
+        m_hopper = hopper;
+        m_duration = duration;
+        addRequirements(m_hopper);
+    }
+
+    @Override
+    public void initialize() {
+        m_timer.reset();
+        m_timer.start();
+        m_hopper.setHopperExtend();
+    }
+
+    @Override
+    public void execute() {
+        m_hopper.setFuelGrabberSpeed();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        m_hopper.stopFuelGrabber();
+        m_timer.stop();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
+
+}
